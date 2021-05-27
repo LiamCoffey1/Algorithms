3e2c162e8d84414c9b5af5122857fcb48efc066d
LiamCoffey1 <liamcoffeyandroid@gmail.com>
2021-05-27 21:44:15 +0100

    g4g challenges

--- src/TreeTraversal.java
+++ src/TreeTraversal.java
@@ -14,10 +14,24 @@ class TreeNode {
 }
 
 
+
+
+
 /**
  * Methods for traversing a binary tree
  */
 public class TreeTraversal {
+	
+	class Node {
+		Node left, right;
+		int value;
+
+		Node(int value, Node left, Node right) {
+			this.left = left;
+			this.right = right;
+			this.value = value;
+		}
+	}
 
 	static void levelOrder(TreeNode node, int height) {
 		for (int i = 1; i < height; i++) {
@@ -36,6 +50,37 @@ public class TreeTraversal {
 			printLevel(node.right, level - 1);
 		}
 	}
+	
+	boolean isIdentical(Node root1, Node root2)
+	{
+	    if(root1 == null && root2 == null)
+	        return true;
+	    if((root1 == null && root2 != null) || (root2 == null && root1 == null))
+	        return false;
+	    if(root1.value == root2.value && 
+	    (isIdentical(root1.left, root2.left) &&
+	    isIdentical(root1.right, root2.right)))
+	        return true;
+	   return false;
+	}
+	
+	int getSum(Node root) {
+        if(root == null)
+            return 0;
+        else return getSum(root.left) + root.value + getSum(root.right);
+    }
+    boolean isSumTree(Node root) {
+        // Is leaf node
+        if(root == null || (root.left == null || root.right == null))
+            return true;
+        int left_sum = getSum(root.left);
+        int right_sum = getSum(root.right);
+        // If node and all children satisfy propert, true
+        if((left_sum + right_sum == root.value) &&
+            isSumTree(root.left) && isSumTree(root.right))
+            return true;
+        return false;
+    }
 
 	static void inOrder(TreeNode  node) {
 		if (node == null)
